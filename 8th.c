6c0af52fe5a046a511a950d6eb46e19b3668a45a
BinaryCoder-Git <youknow.sharma07@gmail.com>
2026-02-10 15:51:17 +0530

    Add files via upload

--- 8th.c
+++ 8th.c
@@ -1,13 +1,16 @@
-#include<stdio.h>
-int main()
-{
-    int a;
-    printf("enter one no:");
-    scanf("%d",&a);
-    if(a%2==0)
-    printf("the no. is even");
-    else 
-    printf("the no. is odd");
-    return 0;
+#include <stdio.h>
 
+int main() {
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
 }
