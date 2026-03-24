32ff8332cf78bda34a931e69ac6cdc07bbb4f583
BinaryCoder-Git <youknow.sharma07@gmail.com>
2026-03-24 14:22:01 +0530

    Add files via upload

---
+++ FIND ODD AND EVEN NO. IN ARRAY.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, evenCount = 0, oddCount = 0;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+
+    printf("\nTotal Even numbers: %d", evenCount);
+    printf("\nTotal Odd numbers: %d\n", oddCount);
+
+    return 0;
+}
